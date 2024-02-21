65aa84182c9aed70ff1548b6200bb8ba51c00e82
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-02-21 13:28:34 +0100

    Update relation and index row

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -1,5 +1,6 @@
 class Taxonomy < ApplicationRecord
   belongs_to :editor
+  has_and_belongs_to_many :corpora
 
   validates :title, :editor_id, presence: true
   validates :title, uniqueness: { scope: :editor_id }
