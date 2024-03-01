dfee1f38221245191ed1c3c9efc6a1948b391c95
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-03-01 11:35:46 +0100

    Refactor of app/sidekiq/import_csv_job.rb and add relation to corpus

--- app/sidekiq/import_csv_job.rb
+++ app/sidekiq/import_csv_job.rb
@@ -4,11 +4,17 @@ class ImportCsvJob < ActiveJob::Base
   queue_as :default
 
   def perform(blob_key, corpus_id)
+    return if Corpus.find(corpus_id).nil?
+
     csv_url = ActiveStorage::Blob.service.send(:path_for, blob_key)
 
     CSV.foreach(csv_url, headers: true).each.with_index(1) do |row, row_index|
-      row_index += 1
-      SaveTextLineJob.perform_later(corpus_id, { content: row["content"], external_id: row["external_id"], position: row_index })
+      next if row["content"].blank?
+
+      SaveTextLineJob.perform_later(
+        corpus_id,
+        { content: row["content"], external_id: row["external_id"], position: row_index }
+      )
     end
   end
 end
