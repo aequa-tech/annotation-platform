c88e0d70526d51d34d0ca476e9af994160cf36c3
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-03-06 10:34:37 +0100

    Changes after CodeRabbit review

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+  # Use :contabo for storing files on S3 storage. (see config/storage.yml for options).
   config.active_storage.service = :contabo
 
   # Mount Action Cable outside main process or domain.
