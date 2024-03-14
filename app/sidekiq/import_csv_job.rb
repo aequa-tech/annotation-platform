0edc7469d440c30b70b4b06ce3e9452d10017818
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-03-14 16:03:48 +0100

    Add initializer and correct Job

--- app/sidekiq/import_csv_job.rb
+++ app/sidekiq/import_csv_job.rb
@@ -6,15 +6,15 @@ class ImportCsvJob < ActiveJob::Base
   def perform(blob_key, corpus_id)
     return if Corpus.find(corpus_id).nil?
 
-    csv_url = ActiveStorage::Blob.service.send(:path_for, blob_key)
+    ActiveStorage::Blob.find_by(key: blob_key).open do |temp_file|
+      CSV.foreach(temp_file, headers: true).each.with_index(1) do |row, row_index|
+        next if row["content"].blank?
 
-    CSV.foreach(csv_url, headers: true).each.with_index(1) do |row, row_index|
-      next if row["content"].blank?
-
-      SaveTextLineJob.perform_later(
-        corpus_id,
-        { content: row["content"], external_id: row["external_id"], position: row_index }
-      )
+        SaveTextLineJob.perform_later(
+          corpus_id,
+          { content: row["content"], external_id: row["external_id"], position: row_index }
+        )
+      end
     end
   end
 end
