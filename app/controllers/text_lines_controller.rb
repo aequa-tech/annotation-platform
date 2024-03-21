b7222739d58ee43a7c6ad29ea55aa111736bdea8
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-03-21 10:32:52 +0100

    Fix after review

--- app/controllers/text_lines_controller.rb
+++ app/controllers/text_lines_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class TextLinesController < ApplicationController
-  before_action :set_task
+  before_action :set_task, only: %i[index show]
 
   def show
     @text_line = TextLine.find(params[:id])
   end
 
   def index
-    @text_lines = @task.text_lines.order(position: :asc)
+    @text_lines = @task.text_lines.includes(:tasks).order(position: :asc)
   end
 
   private
--- app/models/text_line.rb
+++ app/models/text_line.rb
@@ -1,6 +1,7 @@
 class TextLine < ApplicationRecord
   belongs_to :corpus
   belongs_to :lines_set, optional: true
+  has_many :tasks, through: :lines_set
 
   validates :content, presence: true
 
