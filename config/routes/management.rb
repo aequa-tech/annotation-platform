007bb15b1c8784291ffca63d9884f114680ce870
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-03-25 10:32:03 +0100

    Make editors edit of email/password work

--- config/routes/management.rb
+++ config/routes/management.rb
@@ -9,8 +9,8 @@ devise_scope :editor do
   get "management/login" => "management/editors/sessions#new", :as => :new_editor_session
   post "management/login" => "management/editors/sessions#create", :as => :editor_session
   delete "management/logout" => "management/editors/sessions#destroy", :as => :destroy_editor_session
-  get "editors/edit" => "management/editors/registrations#edit", :as => :edit_editor_registration
-  put "editors" => "management/editors/registrations#update", :as => :editor_registration
+  get "management/editors/edit" => "management/editors/registrations#edit", :as => :edit_editor_registration
+  put "management/editors" => "management/editors/registrations#update", :as => :editor_registration
 end
 
 namespace "management" do
