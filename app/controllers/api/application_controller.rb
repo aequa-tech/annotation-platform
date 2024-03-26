3d9a29f32e27e418cbcfa7f96c6ad4d153201945
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-03-26 16:03:38 +0100

    Authorize API actions

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,13 +1,23 @@
 module Api
   class ApplicationController < ActionController::API
-    # before_action :authenticate_user!
+    include Pundit::Authorization
 
-    # def authenticate_user!
-    #   head :unauthorized unless current_user
-    # end
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-    # def current_user
-    #   @current_user ||= User.find_by(authentication_token: params[:authentication_token])
-    # end
+    def pundit_user
+      current_annotator
+    end
+
+    private
+
+    def user_not_authorized
+      flash_message = I18n.t("pundit.not_authorized_error")
+
+      respond_to do |format|
+        format.json do
+          render json: { error: flash_message }, status: :forbidden
+        end
+      end
+    end
   end
 end
