51f35a3342f52e5c9751dafa616881e5dd822f0e
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-04-03 09:39:08 +0200

    Style text line page

--- app/javascript/components/Sidebar.js
+++ app/javascript/components/Sidebar.js
@@ -12,16 +12,18 @@ const Sidebar = (props) => {
   }
 
   return (
-    <aside id="aside-main" className="d-flex align-items-stretch align-items-start flex-column aside-end aside-secondary fw-light aside-hide-xs">
-      <div className="clearfix px-3 py-4 mb-1 text-center bg-diff align-self-baseline w-100">
-        <h5 className="h5">Sidebar</h5>
-        <p>Text Line ID: {textLineId}</p>
+    <aside id="aside-main" className="aside-main d-flex align-items-stretch align-items-start flex-column aside-end fw-light aside-hide-xs">
+      <div className="aside-main__header clearfix px-3 py-4 mb-1 text-center bg-diff align-self-baseline w-100">
+        <h5 className="h5">
+          Text Line ID<br/>
+          <span className="h5__bigger">{textLineId}</span>
+        </h5>
       </div>
-      <div className="aside-wrapper scrollable-vertical scrollable-styled-light align-self-baseline h-100 w-100">
+      <div className="aside-main__wrapper scrollable-vertical scrollable-styled-light align-self-baseline h-100 w-100">
         <nav className="nav-deep nav-deep-light nav-deep-hover">
-          <ul className="nav flex-column">
+          <ul className="annotations-list nav flex-column">
             {annotations.map((a) => (
-              <li key={a.id} className="nav-item" data-id={a.id}>
+              <li key={a.id} className="annotations-list__item nav-item" data-id={a.id}>
                 <a className="nav-link d-inline-block" href="#" onClick={() => props.onSelected(a)}>{ a.body[0].value }</a>
                 <button className="fi fi-thrash d-inline-block" onClick={() => deleteAnnotation(a)}></button>
               </li>
