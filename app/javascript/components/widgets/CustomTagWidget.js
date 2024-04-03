fc12f3de01d83515107093cf2f1bcce15a1d6e94
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-04-03 12:13:53 +0200

    Update custom tag widget

--- app/javascript/components/widgets/CustomTagWidget.js
+++ app/javascript/components/widgets/CustomTagWidget.js
@@ -2,10 +2,16 @@ import { useEffect, useState, useRef } from 'react';
 import { GetTaxonomies } from '../services/AnnotationAPI';
 import { CSSTransition } from 'react-transition-group';
 import { CloseIcon } from '@recogito/recogito-client-core/src/Icons';
+import SuggestionsLoaded from './custom_tag_widget/SuggestionsLoaded';
+import PropTypes from "prop-types";
+
+import './CustomTagWidget.scss';
 
 function CustomTagWidget(props) {
   const [tags, setTags] = useState([]);
   const elem = useRef();
+  const [ isDropdownOpen, setIsDropdownOpen ] = useState(false);
+  const [ loadState, setLoadState ] = useState('LOADED'); // LOADING, LOADED, FAILED
 
   const selectedTags = props.annotation?.bodies.filter(body => body.purpose === 'tagging').map(body => body.value) || [];
 
@@ -19,21 +25,22 @@ function CustomTagWidget(props) {
       });
   }, []);
 
-  const handleSelectChange = (event) => {
-    const selected = Array.from(event.target.selectedOptions, option => option.value);
+  const handleSelectChange = (tag) => {
+    if(selectedTags.includes(tag.title)){
+      onDelete(tag.value)
+    } else {
+      props.onAppendBody({
+        type: "TaxonomyBody",
+        purpose: "tagging",
+        value: tag.title
+      });
+    }
 
-    selected.forEach((tag) => {
-      if(selectedTags.includes(tag)){
-        onDelete(tag)
-      } else {
-        props.onAppendBody({
-          type: "TaxonomyBody",
-          purpose: "tagging",
-          value: tag
-        });
-      }
-    });
+    setIsDropdownOpen(false);
   };
+
+  const onToggleDropdown = () =>
+    setIsDropdownOpen(!isDropdownOpen);
 
   /* FROM TAG WIDGET */
   const [ showDelete, setShowDelete ] = useState(false);
@@ -46,7 +53,7 @@ function CustomTagWidget(props) {
   };
 
   const onDelete = tag => evt => {
-    const toRemove = props.annotation.body.find(body => body.purpose === 'tagging', body => body.value === tag);
+    const toRemove = props.annotation.body.filter(body => body.purpose === 'tagging').find(body => body.value === tag);
     evt.stopPropagation();
     props.onRemoveBody(toRemove);
   }
@@ -54,33 +61,55 @@ function CustomTagWidget(props) {
   /* END FROM TAG WIDGET */
 
   return (
-    <div className="r6o-widget r6o-tag">
+    <div className="r6o-widget r6o-semtags taxonomy-widget">
       { selectedTags.length > 0 &&
-        <ul className="r6o-taglist">
-          { selectedTags.map(tag =>
-            <li key={tag} onClick={toggle(tag)}>
-              <span className="r6o-label">{tag}</span>
-              <CSSTransition in={showDelete === tag} timeout={200} classNames="r6o-delete">
-                <span className="r6o-delete-wrapper" onClick={onDelete(tag)}>
-                  <span className="r6o-delete">
-                    <CloseIcon width={12} />
+        <div className="r6o-widget r6o-tag">
+          <ul className="r6o-taglist">
+            { selectedTags.map(tag =>
+              <li key={tag} onClick={toggle(tag)}>
+                <span className="r6o-label">{tag}</span>
+                <CSSTransition in={showDelete === tag} timeout={200} classNames="r6o-delete">
+                  <span className="r6o-delete-wrapper" onClick={onDelete(tag)}>
+                    <span className="r6o-delete">
+                      <CloseIcon width={12} />
+                    </span>
                   </span>
-                </span>
-              </CSSTransition>
-            </li>
-          )}
-        </ul>
+                </CSSTransition>
+              </li>
+            )}
+          </ul>
+        </div>
       }
-      { tags.length > 0 ?
-        <select multiple value={selectedTags} onChange={handleSelectChange}>
-          {tags.map(tag => (
-            <option key={tag.id} value={tag.title}>{tag.title}</option>
-          ))}
-        </select>
-        : null
+      <div
+        className={ isDropdownOpen ? 'r6o-semtags-taglist dropdown-open' : 'r6o-semtags-taglist' }
+        onClick={onToggleDropdown}>
+        <div className="placeholder">
+          <p className="d-inline-block">Click to add taxonomies...</p>
+        </div>
+      </div>
+
+      {isDropdownOpen &&
+        <div className="r6o-semtags-dropdown-container">
+          <div className="r6o-semtags-dropdown">
+            <div className="r6o-semtags-dropdown-top"></div>
+
+            <div className="r6o-semtags-dropdown-bottom">
+              {loadState === 'LOADED' &&
+                <SuggestionsLoaded
+                  suggestions={tags}
+                  onSelectSuggestion={handleSelectChange} /> }
+            </div>
+          </div>
+        </div>
       }
     </div>
   );
 }
+
+CustomTagWidget.propTypes = {
+  annotation: PropTypes.object,
+  onAppendBody: PropTypes.func,
+  onRemoveBody: PropTypes.func
+};
 
 export default CustomTagWidget;
