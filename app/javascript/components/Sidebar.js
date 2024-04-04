7e3e4b2b8df3faff788553eb2ae3a16a86e444db
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-04-04 15:47:58 +0200

    Style card on sidebar

--- app/javascript/components/Sidebar.js
+++ app/javascript/components/Sidebar.js
@@ -12,6 +12,7 @@ const Sidebar = (props) => {
   }
 
   return (
+    <>
     <aside id="aside-main" className="aside-main d-flex align-items-stretch align-items-start flex-column aside-end fw-light aside-hide-xs">
       <div className="aside-main__header clearfix px-3 py-4 mb-1 text-center bg-diff align-self-baseline w-100">
         <h5 className="h5">
@@ -23,15 +24,26 @@ const Sidebar = (props) => {
         <nav className="nav-deep nav-deep-light nav-deep-hover">
           <ul className="annotations-list nav flex-column">
             {annotations.map((a) => (
-              <li key={a.id} className="annotations-list__item nav-item" data-id={a.id}>
-                <a className="nav-link d-inline-block" href="#" onClick={() => props.onSelected(a)}>{ a.body[0].value }</a>
-                <button className="fi fi-thrash d-inline-block" onClick={() => deleteAnnotation(a)}></button>
+              <li className="annotations-list__item nav-item" data-id={a.id}>
+                <a className="nav-link d-inline-block r6o-widget r6o-tag" href="#" onClick={() => props.onSelected(a)}>
+                  <ul className="r6o-taglist">
+                    { a.body.filter(body => body.purpose === 'commenting').map((b) => (
+                      <p>{b.value}</p>
+                    ))}
+                  <ul className="r6o-taglist"></ul>
+                    { a.body.filter(body => body.purpose === 'tagging').map((b) => (
+                      <li><span className="r6o-label">{b.value}</span></li>
+                    ))}
+                  </ul>
+                </a>
+                <button className="fi fi-thrash d-inline-block" onClick={() => deleteAnnotation(a)} />
               </li>
             ))}
           </ul>
         </nav>
       </div>
     </aside>
+    </>
   );
 }
 
