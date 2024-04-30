e11640d38fcc4ff8feda2a5e74a89afba007103d
Alessandra Cannatà <alessandra.cannata@welaika.com>
2024-04-30 17:25:53 +0200

    Annotate models

--- app/models/annotator.rb
+++ app/models/annotator.rb
@@ -21,17 +21,28 @@ end
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  fullname               :string
+#  invitation_accepted_at :datetime
+#  invitation_created_at  :datetime
+#  invitation_limit       :integer
+#  invitation_sent_at     :datetime
+#  invitation_token       :string
+#  invitations_count      :integer          default(0)
+#  invited_by_type        :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  editor_id              :bigint           not null
+#  invited_by_id          :bigint
 #
 # Indexes
 #
-#  index_annotators_on_editor_id  (editor_id)
-#  index_annotators_on_email      (email) UNIQUE
+#  index_annotators_on_editor_id         (editor_id)
+#  index_annotators_on_email             (email) UNIQUE
+#  index_annotators_on_invitation_token  (invitation_token) UNIQUE
+#  index_annotators_on_invited_by        (invited_by_type,invited_by_id)
+#  index_annotators_on_invited_by_id     (invited_by_id)
 #
 # Foreign Keys
 #
